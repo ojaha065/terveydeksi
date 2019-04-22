e37de3f34ab57fbf1a5e4367a2327fde56158a43
Jani Haiko <ojaha065@edu.xamk.fi>
2019-04-22 15:34:02 +0300

    Korjattu ajanvarauksen pvm ja klo + muuta

--- src/app/ajanvaraus-modal/ajanvaraus-modal.page.ts
+++ src/app/ajanvaraus-modal/ajanvaraus-modal.page.ts
@@ -12,7 +12,8 @@ export class AjanvarausModalPage implements OnInit {
   yritys: any = this.navParams.get("yritys"); // Oma tietotyyppi tähän
 
   varaus: any = { // Oma tietotyyppi tähän
-    aikaleima: null,
+    valittuDate: null,
+    valittuDatePinta: null,
     klo: null,
     palveluntarjoaja: {
       id: this.yritys.id,
@@ -23,39 +24,46 @@ export class AjanvarausModalPage implements OnInit {
   };
 
   page1: boolean = true;
+  seuraavaNappiDisabloitu: boolean = false;
 
   pvmValittu = (valittuPvm: Date): void => {
     //console.log(valittuPvm);
     // Tässä kohti pitäisi hakea tiedot, että onko valittuna päivänä vapaata
-    this.varaus.aikaleima = valittuPvm;
+    this.varaus.valittuDate = valittuPvm;
+    this.varaus.valittuDatePinta = `${valittuPvm.getDate()}.${valittuPvm.getMonth()}.${valittuPvm.getFullYear()}`;
   };
   varausSeuraavaNappi = (): void => {
+    this.seuraavaNappiDisabloitu = true;
     if(this.page1){
       this.http.get(`${this.terveydeksi.apiUrl}/omatTiedot?token=${this.terveydeksi.loginToken}`).subscribe((response: object): void => {
         // OK
-        this.varaus.asiakas = response;
+        this.varaus.asiakas = response[0];
+        this.page1 = false;
+        this.seuraavaNappiDisabloitu = false;
       },(error: HttpErrorResponse): void => {
         // Virhe
+        this.terveydeksi.toast(`Virhekoodi ${error.status}! Yritä myöhemmin uudelleen.`)
         console.error(`${error.status}: ${error.statusText}`);
+        this.seuraavaNappiDisabloitu = false;
       });
 
       let kellonaika = new Date(this.varaus.klo); // Säädetään aikaleimaa valitun kellonajan mukaan
-      this.varaus.aikaleima.setHours(kellonaika.getHours(),kellonaika.getMinutes());
-      this.page1 = false;
+      this.varaus.valittuDate.setHours(kellonaika.getHours(),kellonaika.getMinutes());
     }
     else{
       this.http.post(`${this.terveydeksi.apiUrl}/tallennaAjanvaraus`,{
         token: this.terveydeksi.loginToken,
         yritysID: this.varaus.palveluntarjoaja.id,
-        timestamp: this.varaus.aikaleima.getTime() / 1000 // UNIX-aikaleima
-      }).subscribe((response: HttpResponse<any>): void => {
+        timestamp: this.varaus.valittuDate.getTime() / 1000 // UNIX-aikaleima
+      }).subscribe((): void => {
         // OK
         this.terveydeksi.toast("Varaus tallennettu onnistuneesti.")
         this.page1 = true;
         this.modalController.dismiss();
       },(error: HttpErrorResponse): void => {
         console.error(`${error.status}: ${error.statusText}`);
-        this.terveydeksi.toast(`Virhekoodi ${error.status}! Varausta ei tallennettu. Yritä myöhemmin uudelleen.`)
+        this.terveydeksi.toast(`Virhekoodi ${error.status}! Varausta ei tallennettu. Yritä myöhemmin uudelleen.`);
+        this.seuraavaNappiDisabloitu = false;
       });
     }
   };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
+import { registerLocaleData } from "@angular/common";
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from "@angular/common/http";
+
+import localeFi from "@angular/common/locales/fi";
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Geolocation } from "@ionic-native/geolocation/ngx";
+import { CallNumber } from '@ionic-native/call-number/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +18,7 @@ import { AppComponent } from './app.component';
 import { YritysModalPageModule } from "./yritys-modal/yritys-modal.module";
 import { AjanvarausModalPageModule } from './ajanvaraus-modal/ajanvaraus-modal.module';
 
-import { CallNumber } from '@ionic-native/call-number/ngx';
+registerLocaleData(localeFi);
 
 @NgModule({
   declarations: [AppComponent],
